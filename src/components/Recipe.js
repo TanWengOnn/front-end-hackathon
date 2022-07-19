dce955aa02f6c9abcffdbeba97958de24bd0605d
lashween <lashweenraj@besquare.com.my>
2022-07-19 22:11:38 +0800

    modify fav button into heart img

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,57 +1,90 @@
 // import { v4 as uuidv4} from "uuid"
 // import { useEffect, useState } from "react";
-import { db } from "../firebase-config"
-import { collection, addDoc, doc, deleteDoc  } from "firebase/firestore"
-import { Link } from "react-router-dom" 
+import { addDoc, collection, deleteDoc, doc } from "firebase/firestore";
+import { Link } from "react-router-dom";
+import { db } from "../firebase-config";
+import { ReactComponent as GreyHeart } from "../images/grey-heart.svg"; // Imported svg from file as a component
+import { ReactComponent as RedHeart } from "../images/red-heart.svg"; // Imported svg from file as a component
+import "./Recipe.css";
 
 const Recipe = ({ label, image, url, ingredients, favourite, id }) => {
-    // const { label, image, url, ingredients } = recipe.recipe;
-    const userCollectionRef = collection(db, "favourite")
+  // const { label, image, url, ingredients } = recipe.recipe;
+  const userCollectionRef = collection(db, "favourite");
 
-    // Add favourite to firebase DB
-    const createRecipe = async () => {
-        await addDoc(userCollectionRef, {label: label, image: image, url: url, ingredients: ingredients, favourite: true});
-    }
+  // Add favourite to firebase DB
+  const createRecipe = async () => {
+    await addDoc(userCollectionRef, {
+      label: label,
+      image: image,
+      url: url,
+      ingredients: ingredients,
+      favourite: true,
+    });
+  };
 
-    const deleteRecipe = async (id) => {
-        const recipeDoc = doc(db, "favourite", id);
-        await deleteDoc(recipeDoc);
-        window.location.reload();
-    }
+  const deleteRecipe = async (id) => {
+    const recipeDoc = doc(db, "favourite", id);
+    await deleteDoc(recipeDoc);
+    window.location.reload();
+  };
 
-    //console.log("food: "+ingredients.map(ingredient => ingredient.foodId))
-    return ( 
-        <div>
-            {/* shows all of the recipe details */}
-            
-            <h2> {label} </h2>
-            <Link to="/recipe-details" 
-                state={{label: label, image: image, url: url, 
-                ingredients: ingredients, favourite: favourite,id: id }}>
-                    <img src={image} alt={label} />
-            </Link>
-            <a href={url} target="_blank" rel="noopener noreferrer">
-                URL
-            </a>
-            { !favourite && <button onClick={createRecipe}>Add to favourite</button>}
-            { favourite && <button onClick={() => {deleteRecipe(id)}}>Remove from favourite</button>}
-           
-            {/* shows the ingredients */}
-            {
-                //--Ingredients can be seen by clicking the image--//
-                //--Uncomment to show the ingredients--//
-                // ingredients.map(ingredient => 
-                //     <ul key={uuidv4()}>
-                //         <li>{ingredient.text}</li>
-                //         <li>Weight - {ingredient.weight}</li>
-                //         {/* <li>ID - {ingredient.foodId}</li> */}
-                //     </ul>
-                // )
-            }
+  //console.log("food: "+ingredients.map(ingredient => ingredient.foodId))
+  return (
+    <div className="main-container">
+      {/* shows all of the recipe details */}
 
-            {/* <Link to="/recipe-detail"><button>More Detail</button></Link> */}
+      <h2> {label} </h2>
+      <div className="image-container">
+        <Link
+          className="image"
+          to="/recipe-details"
+          state={{
+            label: label,
+            image: image,
+            url: url,
+            ingredients: ingredients,
+            favourite: favourite,
+            id: id,
+          }}
+        >
+          <img src={image} alt={label} />
+        </Link>
+        <div className="icon-container">
+          {!favourite && (
+            <>
+              {" "}
+              {/* Replaced favourite button with icon */}
+              <RedHeart className="icon" onClick={createRecipe} />
+            </>
+          )}
+          {favourite && (
+            <>
+              <GreyHeart
+                className="icon"
+                onClick={() => {
+                  deleteRecipe(id);
+                }}
+              />
+            </>
+          )}
         </div>
-     );
-}
- 
+      </div>
+      {/* shows the ingredients */}
+      {
+        //--Ingredients can be seen by clicking the image--//
+        //--Uncomment to show the ingredients--//
+        // ingredients.map(ingredient =>
+        //     <ul key={uuidv4()}>
+        //         <li>{ingredient.text}</li>
+        //         <li>Weight - {ingredient.weight}</li>
+        //         {/* <li>ID - {ingredient.foodId}</li> */}
+        //     </ul>
+        // )
+      }
+
+      {/* <Link to="/recipe-detail"><button>More Detail</button></Link> */}
+    </div>
+  );
+};
+
 export default Recipe;
