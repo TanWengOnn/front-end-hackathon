dfbe5a7f305597f6ea050f0e00d6d3b1a623010d
CTWO2000 <tan.wengonn@hotmail.com>
2022-07-20 01:08:24 +0800

    fixed firebase duplicates

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,6 +1,6 @@
 // import { v4 as uuidv4} from "uuid"
-// import { useEffect, useState } from "react";
-import { addDoc, collection, deleteDoc, doc } from "firebase/firestore";
+import { useEffect, useState } from "react";
+import { addDoc, collection, deleteDoc, doc, getDocs } from "firebase/firestore";
 import { Link } from "react-router-dom";
 import { db } from "../firebase-config";
 import { ReactComponent as GreyHeart } from "../images/grey-heart.svg"; // Imported svg from file as a component
@@ -10,16 +10,39 @@ import "./Recipe.css";
 const Recipe = ({ label, image, url, ingredients, favourite, id }) => {
   // const { label, image, url, ingredients } = recipe.recipe;
   const userCollectionRef = collection(db, "favourite");
+  const [favouriteRecipes, setFavouriteRecipes] = useState([])
+
+  const getRecipes = async () => {
+    const data = await getDocs(userCollectionRef);
+    const dbLabels =  data.docs.map(doc => ({...doc.data(), id: doc.id}))
+    setFavouriteRecipes(dbLabels.map(recipe => recipe.label))
+  }
+
+  useEffect(()=>{
+    getRecipes();
+  }, [])
+  
 
   // Add favourite to firebase DB
   const createRecipe = async () => {
-    await addDoc(userCollectionRef, {
-      label: label,
-      image: image,
-      url: url,
-      ingredients: ingredients,
-      favourite: true,
-    });
+    getRecipes();
+
+    // checks if the label already exist in the database 
+    if (!favouriteRecipes.includes(label)){
+      await addDoc(userCollectionRef, {
+        label: label,
+        image: image,
+        url: url,
+        ingredients: ingredients,
+        favourite: true,
+      });
+      
+    }
+    // else{
+    //   console.log("entry exist")
+    //   console.log(favouriteRecipes)
+    // }
+    
   };
 
   const deleteRecipe = async (id) => {
