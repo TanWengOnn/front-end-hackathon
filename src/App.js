85d18835b3ca6b4e6b477790c20a8fe596e35778
Sharifah <sharifah@besquare.com.my>
2022-07-20 11:19:24 +0800

    Styling for App

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
-import './App.css';
-import Home from "./components/Home"
-import NavBar from './components/NavBar';
-import CuisineType from './components/CuisineType';
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
-import RecipeDetail from './components/RecipeDetail';
+import "./App.css";
+import Home from "./components/Home";
+import NavBar from "./components/NavBar";
+import CuisineType from "./components/CuisineType";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import RecipeDetail from "./components/RecipeDetail";
 
 function App() {
-
   return (
-    <Router >
+    <Router>
       <div className="App">
         <NavBar />
         <Routes>
-          <Route exact path="/" element={<Home />}/>
+          <Route exact path="/" element={<Home />} />
           {/* Copy paste the line below for more cuisine type */}
-          <Route exact path="/indian" element={<CuisineType cuisineType={"Indian"}/>}/>
-          <Route exact path="/chinese" element={ <CuisineType cuisineType={"Chinese"}/>}/>
+          <Route
+            exact
+            path="/indian"
+            element={<CuisineType cuisineType={"Indian"} />}
+          />
+          <Route
+            exact
+            path="/chinese"
+            element={<CuisineType cuisineType={"Chinese"} />}
+          />
           {/* <Route exact path="/recipe-detail" element={ <RecipeDetail /> }/> */}
         </Routes>
       </div>
