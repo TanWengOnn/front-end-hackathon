c7e8e225f6df8355b542adaca0f11a4220cbaf7c
Sharifah <sharifah@besquare.com.my>
2022-07-20 21:27:14 +0800

    css for favourite

--- src/components/Favourite.js
+++ src/components/Favourite.js
@@ -1,54 +1,55 @@
 import { useEffect, useState } from "react";
-import { db } from "../firebase-config"
-import { collection, getDocs } from "firebase/firestore"
+import { db } from "../firebase-config";
+import { collection, getDocs } from "firebase/firestore";
 import Recipe from "./Recipe";
-import { v4 as uuidv4} from "uuid"
+import { v4 as uuidv4 } from "uuid";
+import "./Favourite.css";
 
 const Favourite = () => {
-    // Getting Database "table"
-    const userCollectionRef = collection(db, "favourite")
-    // State to hold the database information
-    const [favouriteRecipes, setFavouriteRecipes] = useState([])
+  // Getting Database "table"
+  const userCollectionRef = collection(db, "favourite");
+  // State to hold the database information
+  const [favouriteRecipes, setFavouriteRecipes] = useState([]);
 
-    // Getting Database information on startup/refresh
-    useEffect(() => {
-        const getRecipes = async () => {
-            // Error Handling
-            try{
-                const data = await getDocs(userCollectionRef);
-                setFavouriteRecipes(data.docs.map(doc => ({...doc.data(), id: doc.id})))
-            }catch (error){
-                console.error(error)
-            }
-            
-        }
+  // Getting Database information on startup/refresh
+  useEffect(() => {
+    const getRecipes = async () => {
+      // Error Handling
+      try {
+        const data = await getDocs(userCollectionRef);
+        setFavouriteRecipes(
+          data.docs.map((doc) => ({ ...doc.data(), id: doc.id }))
+        );
+      } catch (error) {
+        console.error(error);
+      }
+    };
 
-        getRecipes()
-    }, []);
+    getRecipes();
+  }, []);
 
-    //console.log(favouriteRecipes)
+  //console.log(favouriteRecipes)
 
-    return ( 
-        <div>
-            <h2>Favourite</h2>
+  return (
+    <div className="favourite_container">
+      <h1>Favourite</h1>
+      {favouriteRecipes.length === 0 && <h2>No Favourites</h2>}
+      <div className="favourite">
+        {/* Recipe "Card" in the Favourite page */}
+        {favouriteRecipes.map((recipe) => (
+          <Recipe
+            key={uuidv4()}
+            label={recipe.label}
+            image={recipe.image}
+            url={recipe.url}
+            ingredients={recipe.ingredients}
+            id={recipe.id}
+            favourite={recipe.favourite}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
 
-            {favouriteRecipes.length === 0 && <h2>No Favourites</h2>}
-
-            {/* Recipe "Card" in the Favourite page */}
-            {favouriteRecipes.map(recipe => (
-               <Recipe key={uuidv4()}
-               label={recipe.label} 
-               image={recipe.image} 
-               url={recipe.url} 
-               ingredients={recipe.ingredients} 
-               id={recipe.id} 
-               favourite={recipe.favourite} 
-                />
-            ))}
-
-
-        </div>
-     );
-}
- 
 export default Favourite;
