6d3dbfa8f05093076989b42945535cafed8baebf
Sharifah <sharifah@besquare.com.my>
2022-07-20 11:22:25 +0800

    Styling for Recipe display

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,29 +1,25 @@
-import { v4 as uuidv4} from "uuid"
+import { v4 as uuidv4 } from "uuid";
+import "./Recipe.css";
 
-const Recipe = ({ recipe }) => {
-    const { label, image, url, ingredients } = recipe.recipe;
+const Recipe = ({ title, calories, image, ingredients }) => {
+  //   const { label, image, url, ingredients } = recipe.recipe;
 
-    return ( 
-        <div>
-            {/* shows all of the recipe details */}
-            <h2> {label} </h2>
-            <img src={image} alt={label} />
-            <a href={url} target="_blank" rel="noopener noreferrer">
-                URL
-            </a>
-            {/* shows the ingredients */}
-            {
-                ingredients.map(ingredient => 
-                    <ul key={uuidv4()}>
-                        <li>{ingredient.text}</li>
-                        <li>Weight - {ingredient.weight}</li>
-                    </ul>
-                )
-            }
+  return (
+    <div className="recipe">
+      <h1>{title}</h1>
+      <ol>
+        {/* shows the ingredients */}
+        {/* {ingredients.map((ingredient) => (
+          <li>{ingredient.text}</li>
+        ))} */}
+      </ol>
 
-            {/* <Link to="/recipe-detail"><button>More Detail</button></Link> */}
-        </div>
-     );
-}
- 
+      {/* <p>Calories : {calories}</p> */}
+
+      <img className="image" src={image} alt="" />
+      {/* <Link to="/recipe-detail"><button>More Detail</button></Link> */}
+    </div>
+  );
+};
+
 export default Recipe;
